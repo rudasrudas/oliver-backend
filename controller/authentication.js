a8adc524698a64c4ffbfc4c0310ccd7e5201a6c9
rudasrudas <zdanaviciusjustas1@gmail.com>
2022-11-28 11:20:05 +0000

    Ana's model DB changes

---
+++ controller/authentication.js
@@ -0,0 +1,16 @@
+// const Repository = require('./repository');
+// const Services = require('./service');
+
+// module.exports = function(app){
+//     //////////////////////////////
+//     //        ENDPOINTS         //
+//     //////////////////////////////
+
+//     //Checks if the element received actually exists and returns the appropriate response.
+//     function ExistCheck(element, res, title){
+//         if (element === null)
+//             return res.status(404).send(`${title} could not be retrieved. An unexpected error occured.`);
+//         else 
+//             return res.json(element);
+//     }
+// }
---
+++ model/recurring_expense.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient;
+
+var url ="mongodb+srv://dboliver:dboliver123@cluster0.2vxjkke.mongodb.net/?retryWrites=true&w=majority";
+
+MongoClient.connect(url, function(err, db)
+{
+    if(err) throw err;
+
+    var dbo =db.db("dboliver")
+
+    dbo.createCollection("Recurring_Expense", function(err, res){
+
+        if(err) throw err
+        console.log("collection created");
+        db.close();
+
+    })
+})
