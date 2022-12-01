f80a44d4b92d943d9988af3ee9ccc693696cabb2
rudasrudas <zdanaviciusjustas1@gmail.com>
2022-12-01 13:13:53 +0000

    More endpoints

--- model/subscriber.js
+++ model/subscriber.js
@@ -1,18 +1,9 @@
-var MongoClient = require('mongodb').MongoClient;
+const mongoose = require("mongoose");
 
-var url ="mongodb+srv://dboliver:dboliver123@cluster0.2vxjkke.mongodb.net/?retryWrites=true&w=majority";
-
-MongoClient.connect(url, function(err, db)
-{
-    if(err) throw err;
-
-    var dbo =db.db("dboliver")
-
-    dbo.createCollection("Subscriber", function(err, res){
-
-        if(err) throw err
-        console.log("collection created");
-        db.close();
-
-    })
+const subscriberSchema = new mongoose.Schema({
+    
+    email: { type: String, unique: true },
+   
 })
+
+module.exports = mongoose.model("subscriber", subscriberSchema);
