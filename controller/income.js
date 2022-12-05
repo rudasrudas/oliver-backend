9b8beb18f171991d6565700d12b407bc39036b6f
annkarinacaro <anax1410@stud.kea.dk>
2022-12-05 14:43:54 +0100

    income/delete/post test

---
+++ controller/income.js
@@ -0,0 +1,36 @@
+const auth = require("../service/auth");
+const Earnings = require('../model/earnings');
+
+module.exports = function(app){
+
+     //get all households - for testing purposes
+  app.get("/income", async (req, res) => {
+    try {
+      const income = await Earnings.find();
+
+      if(income != null){
+        res.status(200).json(income);
+      }          
+    } catch (err) {
+        console.log(err);
+        res.status(400).send("Error occured while retrieving income data");
+    }
+});
+
+    app.post('/income', auth, async (req, res) => {
+        try {
+            const earnings = req.earnings;
+      
+            await Earnings.findByIdAndUpdate(earnings._id, {
+                amount: req.body.amount,
+                month: req.body.month,
+
+            })
+      
+            res.redirect('/income')
+        } catch (err) {
+            res.status(500).send(err)
+        }
+      })
+
+}
