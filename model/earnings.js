adde2f94458bd8b8008a4112d7737879be49e735
aiste334 <aistekst344@gmail.com>
2022-12-08 15:10:42 +0100

    some changes

--- model/earnings.js
+++ model/earnings.js
@@ -2,9 +2,15 @@ const mongoose = require("mongoose");
 
 const earningsSchema = new mongoose.Schema({
 <<<<<<< HEAD
+<<<<<<< HEAD
     userId: { type: String },
     amount: { type: Number },
     month: {type: Date}
+=======
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
+    amount: {type: Number},
+    month: {type: Date, default: Date.now}
+>>>>>>> 7358b7881fff526d668033340e269077bd126258
 =======
     userId: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
     amount: {type: Number},
